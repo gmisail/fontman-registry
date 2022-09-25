6115b3bd20b1b429b7b571705e917300b41f0ff4
Graham Misail <gmisail64@gmail.com>
2022-09-25 17:33:39 -0400

    Configure database, migration settings

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,15 +1,21 @@
 package db
 
 import (
+	"context"
 	"entgo.io/ent/dialect"
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/gmisail/fontman/registry/ent"
+	"github.com/gmisail/fontman/registry/ent/migrate"
+
 	"log"
 )
 
+/*
+	Create a database connection, setup migration configuration.
+*/
 func CreateConnection() *ent.Client {
-	client, err := ent.Open(dialect.SQLite, "registry.db")
+	client, err := ent.Open(dialect.SQLite, "registry.db?mode=memory&cache=shared&_fk=1")
 
 	if err != nil {
 		log.Fatal(err)
@@ -18,9 +24,17 @@ func CreateConnection() *ent.Client {
 
 	defer client.Close()
 
-	/*
-		run migrations...
-	*/
+	ctx := context.Background()
+
+	migrateErr := client.Schema.Create(
+		ctx,
+		migrate.WithDropIndex(true),
+		migrate.WithDropColumn(true),
+	)
+
+	if migrateErr != nil {
+		log.Fatal(migrateErr)
+	}
 
 	return client
 }
