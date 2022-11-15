c61326ecd2eb6a90a1c837bc6c6c8a054ec6fcb6
Graham Misail <gmisail64@gmail.com>
2022-11-15 15:22:35 -0500

    Ignore internal font style fields (id, family id)

--- pkg/model/style.go
+++ pkg/model/style.go
@@ -3,8 +3,8 @@ package model
 import "github.com/google/uuid"
 
 type FontStyle struct {
-	Id     uuid.UUID `db:"id"`
-	Family string    `db:"family"`
-	Type   string    `db:"type"`
-	URL    string    `db:"url"`
+	Id     uuid.UUID `db:"id" json:"-"`
+	Family string    `db:"family" json:"-"`
+	Type   string    `db:"type" json:"type"`
+	URL    string    `db:"url" json:"url"`
 }
